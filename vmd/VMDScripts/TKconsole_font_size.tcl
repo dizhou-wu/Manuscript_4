9e6a5a03058c4beacf44579bd53f7638d7b4f481
dizhou-wu <43738042+dizhou-wu@users.noreply.github.com>
2023-05-10 14:50:49 -0400

    Add files via upload

---
+++ vmd/VMDScripts/TKconsole_font_size.tcl
@@ -0,0 +1,23 @@
+#####
+#Name of Script:Changing the default font size of VMD TKConsole
+#Date: Tue Oct 20 13:56:10 EDT 2015
+#Name: Ryan Melvin
+#####
+# 
+#####
+#Example call to tcl script
+#vmd input.pdb -dispdev text -e AutoPsf.tcl 
+#Separate words im atomselection with underscores
+#####
+#
+#####
+#ToDo: Add actual options
+#####
+#Credit: Based on a script sent to me by Jiajie Xiao 
+#####
+#
+#####
+#Procedures to Execute
+#####
+
+tkcon font Courier 24
